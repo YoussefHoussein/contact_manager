97ec5e01833df7b52e8f2148ad72b8ffb24edfec
unknown <youssefhoussein701@gmail.com>
2023-08-05 16:42:06 +0300

    add the edit function

--- contact-manager-backend/app/Http/Controllers/ContactController.php
+++ contact-manager-backend/app/Http/Controllers/ContactController.php
@@ -9,14 +9,15 @@ class ContactController extends Controller
 {
     //
     public function addContact(Request $request){
+        
         $name = $request->name;
         $phone_number = $request->phone_number;
         $latitude = $request->latitude;
         $longitude = $request->longitude;
-
+        
         $contacts = Contact::all();
-
         foreach($contacts as $contact){
+            
             if($contact->name == $name){
                 return response()->json([
                     'status' => 'Failed',
@@ -29,28 +30,33 @@ class ContactController extends Controller
                     'message' => 'Contact phone number already exists',
                 ], 200);
             }
-            else{
-                $address = new Address();
-
-                $address->latitude = $latitude;
-                $address->longitude = $longitude;
-                $address->save();
-
-                $contact = new Contact();
-                $contact->name = $name;
-                $contact->phone_number = $phone_number;
-                $contact->address_id = $address->id;
-                $contact->save();
-
-                return response()->json([
-                    'status' => 'Success',
-                ], 200);
-                }
+            
         }
+        
+            $address = new Address();
+
+            $address->latitude = $latitude;
+            $address->longitude = $longitude;
+            $address->save();
+
+            $contact = new Contact();
+            $contact->name = $name;
+            $contact->phone_number = $phone_number;
+            $contact->address_id = $address->id;
+            $contact->save();
+
+            return response()->json([
+                'status' => 'Success',
+                'name' => $name,
+                'phone_number' => $phone_number,
+                'latitude' => $latitude,
+                'longitude' => $longitude,
+            ], 200);
+            
     }
     public function deleteContact(Request $request){
-        if($request->id){
-            $contact = Contact::find($request->id);
+        if($request->name){
+            $contact = Contact::where("name", $request->name)->first();
             if($contact){
                 $contact->delete();
                 return response()->json([
@@ -64,6 +70,49 @@ class ContactController extends Controller
                 ], 200);
             }
         }
+    }
+    public function editContact(Request $request){
+        $id = $request->id;
+        $name = $request->name;
+        $phone_number = $request->phone_number;
+        $latitude = $request->latitude;
+        $longitude = $request->longitude;
+
+        $contacts = Contact::all();
+        foreach($contacts as $contact){
+            if($contact->name == $name){
+                return response()->json([
+                    'status' => 'Failed',
+                    'message' => 'Contact name already exists',
+                ], 200); 
+            }
+            else if($contact->phone_number == $phone_number){
+                return response()->json([
+                    'status' => 'Failed',
+                    'message' => 'Contact phone number already exists',
+                ], 200);
+            }
+            
+        }
+
+        $contact = Contact::find($id);
+
+        $address = Address::find($contact->address_id);
+        
+        $contact->name = $name;
+        $contact->phone_number = $phone_number;
+        $address->latitude = $latitude;
+        $address->longitude = $longitude;
+        $contact->save();
+        $address->save();
+
+        return response()->json([
+            'status' => 'Success',
+            'name' => $name,
+            'phone_number' => $phone_number,
+            'latitude' => $latitude,
+            'longitude' => $longitude,
+        ], 200);
     }
 
 }
